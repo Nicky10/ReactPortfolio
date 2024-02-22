f6fe7e978bd4d15cec61cda84b4edb2ef0ff4905
Nico Delgado <nicolas.felipedelgado@gmail.com>
2024-02-22 10:15:51 -0500

    Updates

---
+++ precache-manifest.0526366ed899186d33a6a22a56fab19b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82dde7323803cebe69c674c316a3f0ca",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "1bb59c1f49c438579f7a",
+    "url": "/ReactPortfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8f85f1af1f3763a983bd",
+    "url": "/ReactPortfolio/static/css/main.07408c4b.chunk.css"
+  },
+  {
+    "revision": "1bb59c1f49c438579f7a",
+    "url": "/ReactPortfolio/static/js/2.7753f71f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/ReactPortfolio/static/js/2.7753f71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f85f1af1f3763a983bd",
+    "url": "/ReactPortfolio/static/js/main.cd416381.chunk.js"
+  },
+  {
+    "revision": "6d5691a116d9c448951d",
+    "url": "/ReactPortfolio/static/js/runtime-main.33587481.js"
+  }
+]);
